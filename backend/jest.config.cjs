6c59640e782ff2b8487209f01efa39bcf3693445
Glaucivan <glaucivanp2@gmail.com>
2025-11-30 15:42:04 -0300

    feat: adiciona configuração para o DB mock

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -3,4 +3,8 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.test.js"],
   verbose: true,
+  moduleNameMapper: {
+    "^better-sqlite3$": "<rootDir>/test-helpers/better-sqlite3-mock.js",
+  },
+  forceExit: true,
 };
